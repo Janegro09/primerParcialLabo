2c5e5a456e421cc62c6031845481a349bc971947
Janegro09 <javier.sosa099@gmail.com>
2020-10-13 01:22:18 -0300

    Se crean funciones para encontrar el rubro con mas avisos

--- src/informes.c
+++ src/informes.c
@@ -1,10 +1,12 @@
 #include "informes.h"
 
+static void existeRubro(Rubro* pArray,int sizeP,int numRubro);
 
 void inicializar_rubros(Rubro* pArray,int size){
 	for(int i=0;i<size;i++)
 	{
 		pArray[i].isEmpty=1;
+		pArray[i].cant=0;
 	}
 }
 
@@ -60,7 +62,87 @@ int informes_cantAvisosPausados(Publicacion* pArray, int sizeP, int* cantAvisosP
 				cant++;
 			}
 		}
-		cantAvisosPausados=cant;
+		*cantAvisosPausados=cant;
 	}
 	return retorno;
 }
+
+int informes_rubroConMasAvisos(Publicacion* pArray, int sizeP, Rubro* pArrayRubro, int* rubroMasUsado)
+{
+	int retorno=-1;
+	int resultado;
+	int rubro;
+	if(pArray!=NULL && sizeP>0 && rubroMasUsado!=NULL)
+	{
+		inicializar_rubros(pArrayRubro, sizeP);
+		for(int i=0;i<sizeP;i++)
+		{
+			if(pArray[i].estado==0)
+			{
+				existeRubro(pArrayRubro,sizeP,pArray[i].numRubro);
+			}
+		}
+		resultado=obtenerRubroMasUsado(pArrayRubro,sizeP,&rubro);
+		if(resultado==0)
+		{
+
+		}
+		*rubroMasUsado=rubro;
+	}
+	return retorno;
+}
+
+void existeRubro(Rubro* pArray,int sizeP,int numRubro)
+{
+	for(int i=0;i<sizeP;i++)
+	{
+		if(pArray[i].isEmpty!=0 && pArray[i].id==numRubro)
+		{
+			pArray[i].cant++;
+			break;
+		} else {
+			if(pArray[i].isEmpty==0){
+				pArray[i].isEmpty=0;
+				pArray[i].id=numRubro;
+				pArray[i].cant++;
+				break;
+			}
+		}
+	}
+}
+
+int obtenerRubroMasUsado(Rubro* pArray,int sizeP,int* rubroMasUsado)
+{
+	int retorno=-1;
+	int maximo;
+	int rubroMaximo;
+	int bandera=0;
+	if(pArray!=NULL && sizeP>0 && rubroMasUsado!=NULL)
+	{
+		for(int i=0;i<sizeP;i++)
+		{
+			if(pArray[i].isEmpty==0 )
+			{
+				if(bandera==0)
+				{
+					bandera=1;
+					retorno=0;
+					maximo=pArray[i].cant;
+					rubroMaximo=pArray[i].id;
+				} else {
+					if(maximo<pArray[i].cant)
+					{
+						maximo=pArray[i].cant;
+						rubroMaximo=pArray[i].id;
+					}
+				}
+			}
+//			else { break }
+		}
+		*rubroMasUsado=rubroMaximo;
+	}
+	return retorno;
+}
+
+
+
--- src/informes.h
+++ src/informes.h
@@ -17,6 +17,9 @@ typedef struct {
 //void informes_masivo(Cliente arrayClientes, int sizeC,Publicacion arrayPublicaciones, int sizeP,char* clienteAvisos,int* cantAvisosPausados,int* rubroMayor);
 int informes_clienteMasAvisos(Cliente* arrayClientes, int sizeC,Publicacion* arrayPublicaciones, int sizeP, int* id);
 int informes_cantAvisosPausados(Publicacion* arrayPublicaciones, int sizeP, int* cantAvisosPausados);
+int informes_rubroConMasAvisos(Publicacion* pArray, int sizeP, Rubro* pArrayRubro, int* rubroMasUsado);
+int obtenerRubroMasUsado(Rubro* pArray,int sizeP,int* rubroMasUsado);
+
 void inicializar_rubros(Rubro* pArray,int size);
 
 #endif
